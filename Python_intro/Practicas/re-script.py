dc273f9bc7c2c258a60221d789cbf282ed94ab27
davidroth126 <102978783+davidroth126@users.noreply.github.com>
2022-05-04 12:10:14 -0300

    practica

--- Python_intro/Practicas/re-script.py
+++
@@ -1,7 +0,0 @@
-import re
-texto = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet et amet."
-patron = "amet"
-print(re.match (patron,texto))
-print(texto[22:26])
-print (re.search(patron,texto).group())
-print (re.findall(patron,texto))
--- Python_intro/Recursos_anexos/script1_manejo_errores.py
+++
@@ -1,7 +0,0 @@
-
-numero = int (input("Cual es el numero que quiere saber el doble?"))
-
-def doble (numero):
-    resultado = numero * 2
-    return resultado
-  
